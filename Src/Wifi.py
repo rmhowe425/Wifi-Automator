0434cd21ff21be6148d7b1b40ded262f7c99b5d4
richard <rhowe425@gmail.com>
2021-01-14 17:21:22 -0500

    Added multi-threading, packet sniffing, and made improvements to the design.

--- Src/Wifi.py
+++ Src/Wifi.py
@@ -1,8 +1,10 @@
+from pcap import pcap
 from os import geteuid
 from util.IO import IO
-from scapy.all import *
-from netifaces import interfaces
+from threading import Thread
 from subprocess import Popen, PIPE
+from Src.AccessPoint import AccessPoint
+from Src.radtiotap.radiotap import radiotap_parse, ieee80211_parse
 
 ''' 
     Class responsible for automating the process of recording 
@@ -12,77 +14,105 @@ class WiFi:
 
     '''
         Constructor for the WiFi class.
+        @param nic: Network Interface to sniff on
     '''
     def __init__(self):
-        self.NICs = []
+        self.tCount = 99
         self.SSIDs = {}
-        self.device = ''
-        self.deauth = rdpcap('../artifacts/deauth.pcap', count = 1)[0]
+        self.done = False
+        self.buff = [''] * self.tCount
+        self.NICs = self.scanAdapters()
 
     '''
-        Searches for all available NIC's and returns
-        a list of NIC's that can be used in monitor mode.
+        Parses the list of available NIC's and 
+        sets a specified device to monitor mode.
+        @param nic: Specified device to be set to monitor mode. 
+        @return: Name of device set to monitor mode. 
     '''
-    def scanAdapters(self):
-        nic = []
+    def setMonMode(self, nic):
+        device = ''
 
-        for dev in interfaces():
-            if 'lo' not in dev and 'eno' not in dev and 'vm' not in dev:
-                nic.append(dev)
-        self.NICs = nic
-
-    '''
-        Parses the list of available NIC's and sets the
-        first available device to monitor mode. 
-        Sets the value of self.NIC to the name of the NIC
-        in monitor mode. 
-        @return: Boolean value representing success of method execution.
-    '''
-    def setMonMode(self):
-        success = False
-
+        # Make sure that the script is running as root(Needs to be able to perform packet injection).
         if geteuid() != 0:
             IO.writeToErrorLog('You must run this program as root.')
-            return success
 
-        for dev in self.NICs:
-            try:
-                # Shut down the device
-                msg = Popen(['ifconfig', dev, 'down'], stderr = PIPE)
-                msg.communicate() # Not saving output.
+        else:
+            for dev in self.NICs:
+                if dev is nic:
+                    try:
+                        # Shut down the device
+                        msg = Popen(['ifconfig', dev, 'down'], stderr = PIPE)
+                        msg.communicate() # Not saving output.
 
-                # Attempt to put device in monitor mode
-                msg = Popen(['iwconfig', dev, 'mode', 'monitor'], stderr = PIPE)
-                msg.communicate() # Not saving output.
+                        # Attempt to put device in monitor mode
+                        msg = Popen(['iwconfig', dev, 'mode', 'monitor'], stderr = PIPE)
+                        msg.communicate() # Not saving output.
 
-                self.device = dev
-                success = True
+                        device = dev
+                        break
+                    except Exception as e:
+                        IO.writeToErrorLog(e)
+        return device
+
+    '''
+        Filters and stores packets based on specified properties.
+        [*] This method is intended to be ran on multiple dedicated threads.
+        @param Id: Unique identifier given to a single thread. 
+    '''
+    def filterPacket(self, Id):
+        packet = self.buff[Id]
+        while True:
+
+            # Done criteria, kill thread.
+            if self.done:
                 break
 
-            except Exception as e:
-                IO.writeToErrorLog(e)
-            return success
+            # worker thread needs to wait.
+            elif self.buff[Id] == packet:
+                pass
 
-        '''
-            Sends a spoofed deauth packet from the client (CLI) to the access point
-            (AP) in order to generate a 3-way handshake upon reconnection.
-            @param SMAC: MAC of victim that will deauthenticate with the AP.
-            @param DMAC: MAC of AP that will receive victim deauth packet.
-            @return: Boolean value representing the success of the method.
-        '''
-        def sendDeauth(self, SMAC, DMAC):
-            # Receiver MAC (Dst)
-            self.deauth.addr1 = DMAC
-            # Destination MAC
-            self.deauth.addr2 = DMAC
-            # Transmitter MAC (Src)
-            self.deauth.addr3 = SMAC
-            # Source MAC -> BSSID MAC in WireShark, not required in Scapy?
-            self.deauth.addr4 = SMAC
+            # Parse and record packet information.
+            else:
+                packet = self.buff[Id]
+                offset, rt_data = radiotap_parse(packet)
 
-            try:
-                # Send packet on layer 2
-                srp(self.deauth)
-            except Exception as e:
-                IO.writeToErrorLog(e)
+                # Broadcast-type of packet (signifies an access point)
+                if packet[offset] == 0x80:
+                    iEE_offset, ap_info = ieee80211_parse(packet, offset)
+                    start = iEE_offset + 14
 
+                    # SSID
+                    ssid = (packet[start: start + packet[iEE_offset + 13]]).decode()
+                    # Power
+                    pwr = rt_data['dbm_antsignal']
+                    # MAC address
+                    mac = ap_info['addr3']
+
+                    # Add new item to dictionary
+                    if ssid not in self.SSIDs:
+                        self.ap_list[ssid] = AccessPoint(pwr, mac, ssid)
+
+    '''
+        Method used for sniffing packets. 
+        [*] This method is intended to be ran in a dedicated thread 
+            for the entire duration of program execution.
+        @param nic: Device to sniff packets on.
+    '''
+    def sniff(self, nic):
+        Id = 0
+        sniff = pcap(name=nic, snaplen=65535, promisc=True, immediate=True)
+        thread_pool = [Thread(target = self.filterPacket, name=i) for i in range(0, self.tCount)]
+
+        # Execute threads
+        for worker in thread_pool:
+            worker.start()
+
+        try:
+            while True:
+                self.buff[Id] = sniff.__next__()[1]
+                Id = (Id + 1) % 10
+
+        except KeyboardInterrupt:
+            for worker in thread_pool:
+                worker.thread.join()
+            return None
--- util/IO.py
+++ util/IO.py
@@ -6,21 +6,12 @@ from datetime import datetime
 class IO:
 
     '''
-        Writes packets to a file.
-        3-way handshake should be included.
-        @param data: Data containing the 3-way handshake. 
-                     Data may be a scapy instance.
-    '''
-    def writeFile(self, data):
-        return
-
-    '''
         Writes an error message to a log file.
         Each entry has the date and time and is 
         appended to the same file.
         @param error: Error message to be logged.
-        @param path: File path to the file to be written to.  
     '''
+    @staticmethod
     def writeToErrorLog(error):
         file = open('../artifacts/Error Log.txt', 'a+')
         timestamp = ''.join([datetime.today().strftime('%Y-%m-%d-%H:%M:%S'), '\n', '-' * 20, '\n'])
@@ -31,6 +22,7 @@ class IO:
     '''
         Used to clear the error log during testing. 
     '''
+    @staticmethod
     def clearErrorLog(self):
         file = open('../artifacts/Error Log.txt', 'w')
         file.close()
